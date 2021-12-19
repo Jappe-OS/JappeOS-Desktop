acc14d8e2b49a81446d96ae1fd5bb53b798c3c64
Jappe02 <jasper.mannonen@gmail.com>
2021-12-19 18:33:34 +0200

    I think this is a Flutter update

--- jappeos_desktop/ios/Flutter/flutter_export_environment.sh
+++ jappeos_desktop/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/jappe02/snap/flutter/common/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/jappe02/Development/JappeOS/JappeOS/jappeos_desktop"
+export "FLUTTER_ROOT=G:\applications\Flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=H:\JappeOS\1.0\FlutterPrj\simpa_desktop\JappeOS\jappeos_desktop"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
