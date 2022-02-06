2910636282912aab012521cb2374bb718029102a
Jappe02 <jasper.mannonen@gmail.com>
2022-02-06 20:51:22 +0200

    Unnecessary.

--- jappeos_desktop/lib/components/functions.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:jappeos_desktop/components/cwidgets.dart';
-
-class Functions {
-  static void gui_spawn_fullscreen_error_dialog(BuildContext arg0, String arg1) {
-    Navigator.push(
-        arg0, MaterialPageRoute(builder: (context) => JFullscreenErrorDialog(arg0: arg0, arg1: arg1,)));
-  }
-}
