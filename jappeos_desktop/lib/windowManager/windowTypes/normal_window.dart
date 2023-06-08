8a42b181293033a41ddafe7fb5c0ff980eed282e
Jappe02 <69361903+Jappe02@users.noreply.github.com>
2023-06-08 22:16:41 +0300

    Custom window decorations

--- jappeos_desktop/lib/windowManager/windowTypes/normal_window.dart
+++ jappeos_desktop/lib/windowManager/windowTypes/normal_window.dart
@@ -23,24 +23,43 @@ class NormalWindow extends WMWindowType {
   final String title;
   final Image? icon;
   final Widget body;
+  final List<Widget>? decorations;
 
   final WMWindowSize size;
   final bool blur;
 
-  NormalWindow(this.title, this.icon, this.size, this.blur, this.body);
+  final bool _controlButtons = true;
+
+  NormalWindow(this.title, this.icon, this.size, this.blur, this.body, [this.decorations]);
+
+  bool _isDecorated() => decorations != null || (decorations == null ? false : decorations!.isNotEmpty);
+  double get decorationExtent => 10;
 
   @override
   List<Widget> getWindow() {
+    double tbarHeight = _isDecorated() ? WMWindowDragAreaProperties.getDefaultH() + decorationExtent : WMWindowDragAreaProperties.getDefaultH();
+
     return [
+      if (_isDecorated())
+        Positioned(
+          top: decorationExtent / 2,
+          left: 0,
+          right: 0,
+          height: tbarHeight,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: decorations ?? [],
+          ),
+        ),
       Positioned(
-        top: WMWindowDragAreaProperties.getDefaultH(),
+        top: tbarHeight,
         left: 0,
         bottom: 0,
         right: 0,
         child: body,
       ),
       Positioned(
-        top: 6,
+        top: _isDecorated() ? 6 + decorationExtent / 2 : 6,
         left: 10,
         child: DeuiText(isTitle: false, text: title),
       ),
@@ -61,6 +80,10 @@ class NormalWindow extends WMWindowType {
   WMWindowDragAreaProperties? getDragAreaProperties() {
     WMWindowDragAreaProperties a = WMWindowDragAreaProperties()..setDefault();
 
+    if (_isDecorated()) {
+      a.h += decorationExtent;
+    }
+
     return a;
   }
 
@@ -71,6 +94,6 @@ class NormalWindow extends WMWindowType {
 
   @override
   bool hasControlButtons() {
-    return true;
+    return _controlButtons;
   }
 }
